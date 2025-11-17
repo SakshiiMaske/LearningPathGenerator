c65b5f96b56fdb32ca61adabe4a9798cc22e4259
Sakshi <sakshi.maske7148@gmail.com>
2025-11-17 08:47:45 +0530

    header and footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LearningPathComponent } from './Components/learning-path/learning-path.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'learning-path', component: LearningPathComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { HeaderComponent } from './HeaderFooter/header/header.component';
+import { FooterComponent } from './HeaderFooter/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { LearningPathComponent } from './Components/learning-path/learning-path.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    LearningPathComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
